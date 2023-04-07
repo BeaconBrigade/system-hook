9c59edadf11510b56bb070ef6f1f1f54bb5959d3
Ryan <beaconbrigade@gmail.com>
2023-04-06 21:38:50 -0230

    used better Option/Result combinators

--- github-webhook/src/axum.rs
+++ github-webhook/src/axum.rs
@@ -15,8 +15,8 @@ use crate::GithubPayload;
 /// Extract a github event from a request
 #[async_trait]
 impl<S> FromRequest<S, Body> for GithubPayload
-    where
-        S: Send + Sync,
+where
+    S: Send + Sync,
 {
     type Rejection = StatusCode;
 
@@ -26,49 +26,35 @@ impl<S> FromRequest<S, Body> for GithubPayload
         let event: String = req
             .headers()
             .get("X-Github-Event")
-            .map(|v| v.to_str().map_err(|_| StatusCode::BAD_REQUEST))
-            .ok_or(StatusCode::BAD_REQUEST)??
+            .and_then(|v| v.to_str().ok())
+            .ok_or(StatusCode::BAD_REQUEST)?
             .to_string();
         tracing::info!("parsing event: {}", event);
         let guid = req
             .headers()
             .get("X-Github-Delivery")
-            .map(|v| {
-                v.to_str()
-                    .map(|s| Uuid::from_str(s).map_err(|_| StatusCode::BAD_REQUEST))
-                    .map_err(|_| StatusCode::BAD_REQUEST)
-            })
-            .ok_or(StatusCode::BAD_REQUEST)???;
+            .and_then(|v| v.to_str().ok())
+            .and_then(|s| Uuid::from_str(s).ok())
+            .ok_or(StatusCode::BAD_REQUEST)?;
         tracing::debug!(?guid);
         let signature_sha1: Option<String> = req
             .headers()
             .get("X-Hub-Signature")
-            .map(|v| {
-                v.to_str()
-                    .map(str::to_lowercase)
-                    .map_err(|_| StatusCode::BAD_REQUEST)
-            })
-            .transpose()?;
+            .and_then(|v| v.to_str().ok())
+            .map(|v| v.to_lowercase());
         let signature_sha256: Option<String> = req
             .headers()
             .get("X-Hub-Signature-256")
-            .map(|v| {
-                v.to_str()
-                    .map(str::to_lowercase)
-                    .map_err(|_| StatusCode::BAD_REQUEST)
-            })
-            .transpose()?;
+            .and_then(|v| v.to_str().ok())
+            .map(|v| v.to_lowercase());
 
         // get body
         let content_type = req
             .headers()
             .get("content-type")
-            .map(|v| {
-                v.to_str()
-                    .map(str::to_lowercase)
-                    .map_err(|_| StatusCode::BAD_REQUEST)
-            })
-            .ok_or(StatusCode::BAD_REQUEST)??;
+            .and_then(|v| v.to_str().ok())
+            .map(|v| v.to_lowercase())
+            .ok_or(StatusCode::BAD_REQUEST)?;
         tracing::debug!(?content_type);
         let raw_bytes = Bytes::from_request(req, state)
             .await
@@ -90,7 +76,9 @@ impl<S> FromRequest<S, Body> for GithubPayload
         match res {
             Ok(payload) => Ok(payload),
             Err(TokenMissing | HmacCreation) => Err(StatusCode::INTERNAL_SERVER_ERROR),
-            Err(Sha1ParseError | Sha256ParseError | HexParseError | EventParseError) => Err(StatusCode::BAD_REQUEST),
+            Err(Sha1ParseError | Sha256ParseError | HexParseError | EventParseError) => {
+                Err(StatusCode::BAD_REQUEST)
+            }
             Err(NotVerified) => Err(StatusCode::UNAUTHORIZED),
         }
     }
